fcb15126449df93472460b5ebe6c1a3127b620c3
Piumi Godage <piumi.godage@helsinki.fi>
2024-07-18 02:47:31 +0300

    completed upto 3.14

--- part3/phonebook/server/index.js
+++ part3/phonebook/server/index.js
@@ -1,6 +1,9 @@
+require('dotenv').config()
 const express = require('express')
 const morgan = require('morgan');
 const cors = require('cors')
+
+const Person = require('./models/person')
 
 const app = express()
 
@@ -10,29 +13,6 @@ app.use(express.static('dist'))
 
 morgan.token('body', (req) => JSON.stringify(req.body))
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'))
-
-let persons = [
-    {
-        "id": "1",
-        "name": "Arto Hellas",
-        "number": "040-123456"
-    },
-    {
-        "id": "2",
-        "name": "Ada Lovelace",
-        "number": "39-44-5323523"
-    },
-    {
-        "id": "3",
-        "name": "Dan Abramov",
-        "number": "12-43-234345"
-    },
-    {
-        "id": "4",
-        "name": "Mary Poppendieck",
-        "number": "39-23-6423122"
-    }
-]
 
 app.get('/info', (request, response) => {
     response.send(
@@ -52,41 +32,34 @@ app.post('/api/persons', (request, response) => {
         })
     }
 
-    if (persons.find((person) => person.name.toLowerCase() == body.name.toLowerCase())) {
-        return response.status(404).json({
-            error: 'name must be unique'
-        })
-    }
-
-    const person = {
+    const person = new Person({
         name: body.name,
         number: body.number,
-        id: Math.floor(Math.random() * 10000000).toString(),
-    }
+    })
 
-    persons = persons.concat(person)
-
-    response.json(person)
+    person.save().then(savedPerson => {
+        response.json(savedPerson)
+    })
 })
 
 app.get('/api/persons', (request, response) => {
-    response.json(persons)
+    Person.find({}).then(persons => {
+        response.json(persons)
+    })
 })
 
 app.get('/api/persons/:id', (request, response) => {
-    const id = request.params.id
-    const person = persons.find(person => person.id === id)
-    if (person) {
+    const id = request.params.id;
+    Person.findById(id).then(person => {
         response.json(person)
-    } else {
-        response.status(404).end()
-    }
+    })
 })
 
 app.delete('/api/persons/:id', (request, response) => {
     const id = request.params.id
-    persons = persons.filter(person => person.id !== id)
-    response.status(204).end()
+    Person.findByIdAndDelete(id).then(() => {
+        response.status(204).end()
+    })
 })
 
 const PORT = 3001
