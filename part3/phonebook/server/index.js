94942d48e695b034d5bd5b9d88cfdedf6062133e
Piumi Godage <piumi.godage@helsinki.fi>
2024-07-18 13:18:27 +0300

    completed upto 3.22

--- part3/phonebook/server/index.js
+++ part3/phonebook/server/index.js
@@ -1,6 +1,6 @@
 require('dotenv').config()
 const express = require('express')
-const morgan = require('morgan');
+const morgan = require('morgan')
 const cors = require('cors')
 
 const Person = require('./models/person')
@@ -15,85 +15,88 @@ morgan.token('body', (req) => JSON.stringify(req.body))
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'))
 
 app.get('/info', (request, response) => {
+  Person.find({}).then((persons) =>
     response.send(
-        `<div>
-            <p>Phonebook has info for ${persons.length} people</p>
-            <p>${new Date().toString()}</p>
-        </div>`
+      `<div>
+                    <p>Phonebook has info for ${persons.length} people</p>
+                    <p>${new Date().toString()}</p>
+                </div>`
     )
+  )
+
 })
 
 app.post('/api/persons', (request, response, next) => {
-    const body = request.body;
+  const body = request.body
 
-    const person = new Person({
-        name: body.name,
-        number: body.number,
+  const person = new Person({
+    name: body.name,
+    number: body.number,
+  })
+
+  person.save()
+    .then(savedPerson => {
+      response.json(savedPerson)
     })
-
-    person.save()
-        .then(savedPerson => {
-            response.json(savedPerson)
-        })
-        .catch(error => next(error))
+    .catch(error => next(error))
 })
 
 app.put('/api/persons/:id', (request, response, next) => {
-    const id = request.params.id;
-    const body = request.body
+  const id = request.params.id
+  const body = request.body
 
-    const person = {
-        name: body.name,
-        number: body.number,
-    }
+  const person = {
+    name: body.name,
+    number: body.number,
+  }
 
-    Person.findByIdAndUpdate(id, person, { new: true, runValidators: true, context: 'query' })
-        .then(savedPerson => {
-            response.json(savedPerson)
-        })
-        .catch(error => next(error))
+  Person.findByIdAndUpdate(id, person, { new: true, runValidators: true, context: 'query' })
+    .then(savedPerson => {
+      response.json(savedPerson)
+    })
+    .catch(error => next(error))
 })
 
 app.get('/api/persons', (request, response, next) => {
-    Person.find({})
-        .then(persons => {
-            response.json(persons)
-        })
-        .catch(error => next(error))
+  Person.find({})
+    .then(persons => {
+      response.json(persons)
+    })
+    .catch(error => next(error))
 })
 
 app.get('/api/persons/:id', (request, response, next) => {
-    const id = request.params.id;
-    Person.findById(id)
-        .then(person => {
-            response.json(person)
-        })
-        .catch(error => next(error))
+  const id = request.params.id
+  Person.findById(id)
+    .then(person => {
+      response.json(person)
+    })
+    .catch(error => next(error))
 })
 
 app.delete('/api/persons/:id', (request, response, next) => {
-    const id = request.params.id
-    Person.findByIdAndDelete(id)
-        .then(() => {
-            response.status(204).end()
-        })
-        .catch(error => next(error))
+  const id = request.params.id
+  Person.findByIdAndDelete(id)
+    .then(() => {
+      response.status(204).end()
+    })
+    .catch(error => next(error))
 })
 
 const errorHandler = (error, request, response, next) => {
-    console.error(error.message)
+  console.error(error.message)
 
-    if (error.name === 'CastError') {
-        return response.status(400).send({ error: 'malformatted id' })
-    } else if (error.name === 'ValidationError') {
-        return response.status(400).json({ error: error.message })
-    }
+  if (error.name === 'CastError') {
+    return response.status(400).send({ error: 'malformatted id' })
+  } else if (error.name === 'ValidationError') {
+    return response.status(400).json({ error: error.message })
+  }
 
-    next(error)
+  next(error)
 }
 app.use(errorHandler)
 
 const PORT = 3001
 app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`)
+  console.log(`Server running on port ${PORT}`)
 })
--- part3/phonebook/server/mongo.js
+++ part3/phonebook/server/mongo.js
@@ -1,8 +1,8 @@
-const mongoose = require("mongoose")
+const mongoose = require('mongoose')
 
 if (process.argv.length < 3) {
-    console.log('give password as argument')
-    process.exit(1)
+  console.log('give password as argument')
+  process.exit(1)
 }
 
 const password = process.argv[2]
@@ -11,8 +11,8 @@ const url = `mongodb+srv://fullstack:${password}@cluster0.xv1y5rv.mongodb.net/?r
 mongoose.set('strictQuery', false)
 
 const personSchema = new mongoose.Schema({
-    name: String,
-    number: String,
+  name: String,
+  number: String,
 })
 
 const Person = mongoose.model('Person', personSchema)
@@ -21,25 +21,25 @@ const name = process.argv[3]
 const number = process.argv[4]
 
 const main = async () => {
-    mongoose.connect(url)
+  mongoose.connect(url)
 
-    if (name && number) {
-        const newPerson = new Person({
-            name,
-            number
-        })
-        newPerson.save().then(() => {
-            console.log(`added ${name} ${number} to phonebook`)
-            mongoose.connection.close()
-        })
-    } else {
-        Person.find({})
-            .then((persons) => {
-                console.log('phonebook:')
-                persons.forEach((person) => console.log(`${person.name} ${person.number}`))
-                mongoose.connection.close()
-            })
-    }
+  if (name && number) {
+    const newPerson = new Person({
+      name,
+      number
+    })
+    newPerson.save().then(() => {
+      console.log(`added ${name} ${number} to phonebook`)
+      mongoose.connection.close()
+    })
+  } else {
+    Person.find({})
+      .then((persons) => {
+        console.log('phonebook:')
+        persons.forEach((person) => console.log(`${person.name} ${person.number}`))
+        mongoose.connection.close()
+      })
+  }
 }
 
 main()
